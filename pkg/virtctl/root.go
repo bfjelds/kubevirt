0252e5becf7c5018512db9297dd6d4b651a0fe1c
Brian Fjeldstad <bfjelds@microsoft.com>
2023-07-17 14:27:33 -0700

    add save/restore commands to cli

--- pkg/virtctl/root.go
+++ pkg/virtctl/root.go
@@ -22,6 +22,8 @@ import (
 	"kubevirt.io/kubevirt/pkg/virtctl/memorydump"
 	"kubevirt.io/kubevirt/pkg/virtctl/pause"
 	"kubevirt.io/kubevirt/pkg/virtctl/portforward"
+	"kubevirt.io/kubevirt/pkg/virtctl/restore"
+	"kubevirt.io/kubevirt/pkg/virtctl/save"
 	"kubevirt.io/kubevirt/pkg/virtctl/scp"
 	"kubevirt.io/kubevirt/pkg/virtctl/softreboot"
 	"kubevirt.io/kubevirt/pkg/virtctl/ssh"
@@ -100,6 +102,8 @@ func NewVirtctlCommand() (*cobra.Command, clientcmd.ClientConfig) {
 		memorydump.NewMemoryDumpCommand(clientConfig),
 		pause.NewPauseCommand(clientConfig),
 		pause.NewUnpauseCommand(clientConfig),
+		save.NewSaveCommand(clientConfig),
+		restore.NewRestoreCommand(clientConfig),
 		softreboot.NewSoftRebootCommand(clientConfig),
 		expose.NewExposeCommand(clientConfig),
 		version.VersionCommand(clientConfig),
